3903efc381ae76a0d837c8a0a60405a6d2a64d61
Anish Basnet <anishbasnet027@gmail.com>
2022-11-13 13:45:33 +0545

    completed

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,19 @@
 let count = 0
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+console.log(countEl)
 
 function increment() {
     count = count + 1
-    console.log(count)
+    countEl.innerText = count
+}
+
+function save() {
+    let saveE = count + ","
+    saveEl.innerText += saveE
+}
+
+function Clear() {
+    countEl.innerText = 0
+    saveEl.innerText = 0 + ","
 }
